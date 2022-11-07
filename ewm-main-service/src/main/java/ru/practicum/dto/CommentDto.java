84af416e5a1ed2f6cc1b79afabff409403c92263
NatalieFilippi <shabalina.natalya@gmail.com>
2022-11-07 12:50:59 +0300

    Заменила в валидации комментария notEmpty на NotBlank

--- ewm-main-service/src/main/java/ru/practicum/dto/CommentDto.java
+++ ewm-main-service/src/main/java/ru/practicum/dto/CommentDto.java
@@ -4,14 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class CommentDto {
-    @NotNull
+    @NotBlank
     @Size(max = 2000, message = "Максимальный размер комментария 2000 символов!")
     @Size(min = 2, message = "Минимальный размер комментария 2 символа!")
     private String text;
--- ewm-main-service/src/main/java/ru/practicum/dto/NewCommentDto.java
+++ ewm-main-service/src/main/java/ru/practicum/dto/NewCommentDto.java
@@ -4,14 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class NewCommentDto {
-    @NotEmpty(message = "Комментарий не может быть пустым.")
+    @NotBlank(message = "Комментарий не может быть пустым.")
     @Size(max = 2000, message = "Максимальный размер комментария 2000 символов!")
     @Size(min = 2, message = "Минимальный размер комментария 2 символа!")
     private String text;
